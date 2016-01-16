1c2015aa945749eba65f992f539262515766daae
Brett Allen <brettallen777@gmail.com>
2016-01-16 12:13:18 -0500

    Update Logger.java

--- Logger.java
+++ Logger.java
@@ -7,15 +7,18 @@ import java.io.PrintWriter;
 public class Logger {
 	
 	private static String day;
+	private static String date;
 	
-	public Logger(String day)
+	public Logger(String day, String date)
 	{
 		this.day = day;
+		this.date = date;
 	}
 	
 	public void log(String message) throws IOException 
 	{ 
-      PrintWriter out = new PrintWriter(new FileWriter("Front Line Schedule for " + day + ".txt"
+      PrintWriter out = new PrintWriter(new FileWriter("Front Line Schedule for " + day +
+    		  " " + date + ".txt"
     		  , true), true);
       
       out.write(message);
